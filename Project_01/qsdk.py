07f6f314170aae243388d12dacb076b46500e3e6
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2020-02-01 14:09:44 +0100

    code structure and qsdk

---
+++ Project_01/qsdk.py
@@ -0,0 +1,18 @@
+import math
+
+def nCX(k,c,t,b):
+    nc = len(c)
+    if nc == 1:
+        k.gate("cnot",[c[0], t[0]])
+    elif nc == 2:
+        k.toffoli(c[0],c[1],t[0])
+    else:
+        nch = math.ceil(nc/2)
+        c1 = c[:nch]
+        c2 = c[nch:]
+        c2.append(b[0])
+        nCX(k,c1,b,nch+1)
+        nCX(k,c2,t,nch-1)
+        nCX(k,c1,b,nch+1)
+        nCX(k,c2,t,nch-1)
+    return
--- Project_01/tm01.py
+++ Project_01/tm01.py
@@ -1,6 +1,7 @@
 import os
 from math import log2
 from openql import openql as ql
+import qsdk     # from qsdk import nCX
 import qxelarator
 
 # rootDir = os.path.dirname(os.path.realpath(__file__))
@@ -48,44 +49,62 @@ tape = [4, 5, 6, 7, 8, 9]
 circ_width = len(head+read+tape)
 
 p = ql.Program('aritra', platform, circ_width)
-k = ql.Kernel("min_kernel", platform, circ_width)
+k_init = ql.Kernel("init", platform, circ_width)
+k_read = ql.Kernel("read", platform, circ_width)
 
 # 1. Initialize
 #     Tape to all symbol 0
 for i in tape:
-    k.gate('prepz', [i])
-k.gate('x',[4]) # Test Read Head
+    k_init.gate('prepz', [i])
+k_init.gate('x',[4]) # Test Read Head
 #     Current Tape Head to 0
 for i in head:
-    k.gate('prepz', [i])
+    k_init.gate('prepz', [i])
 #     Flag to 0
 #     Read Head to position 0
 for i in read:
-    k.gate('prepz', [i])
+    k_init.gate('prepz', [i])
 #    Write Head to position 0
 #     Current Machine State to state 0
 #     FSM to equal superposition of all FSMs [TBD]
 
 
-# 2. Run machine for n-iterations:
-for tick in range(0, sim_tick):
-    # {q_read} << U_read({q_head, q_tape})
+# k1 = ql.Kernel("min_kernel", platform, circ_width)
+# ck = ql.Kernel('controlled_kernel1', platform, circ_width)
+
+def U_read(k,read,head,tape):
     for cell in range(0,len(tape)):
         print(format(cell, '#05b'), tape[cell])
-        # multi control X
-        
-
-
-    # [{R Head}] = Toffoli [{Head Position}, {Turing Tape}]
+    qsdk.nCX(k,[0, 1, 2],[4],[3])
     # for i in range(0,tsz):
     #     for j in range(0,csz):
     #         k.gate('toffoli', [qbi[0] + i*csz + j, qbi[2] + i , qbi[1] + j])
-    # [{W Head}, {Machine State}, {Move}] = U_fsm [{R Head}, {Machine State}, {FSM}]
+    
+    # multi control X
+    
+    # k1.gate("x", [0])
+    # ck.controlled(k1, [1], [2])
+    
+    return
+
+def U_fsm():
     # k.gate(fsm, [q_rw, q_hp, q_cs, q_sm])  
-    # [{Head Position}, {Error}] = U_move [{Head Position}, {Move}]
-    # Currently ignore: let Head position under/overflow. Trim bits
-    # [{Turing Tape}] = U_write [{Head Position}, {W Head}]
+    return
+    
+def U_write():
     # Reset RW Head
+    return
+ 
+def U_move():
+    # Currently ignore: let Head position under/overflow. Trim bits
+    return
+
+# 2. Run machine for n-iterations:
+for tick in range(0, sim_tick):
+    U_read(k_read,read,head,tape)       # {q_read} << U_read({q_head, q_tape})
+    U_fsm()                             # {q_write, q_state, q_move} << U_fsm({q_read, q_state, q_fsm})
+    U_write()                           # {q_tape} << U_write({q_head, q_write})
+    U_move()                            # {q_head, q_err} << U_move({q_head, q_move})   
 
 # 3. Amplify target sequence using Grover's Gate (QiBAM)
 
@@ -95,7 +114,9 @@ for tick in range(0, sim_tick):
  
 # 5. Choose shortest FSM that doesn't halt or throws error as Kolmogorov Complexity of target DNA sequence
 	
-p.add_kernel(k)
+p.add_kernel(k_init)
+p.add_kernel(k_read)
+# p.add_kernel(k1)
 p.compile()
 
 qasm = p.qasm()
